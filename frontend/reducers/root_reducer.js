79ccce26168e9544cd074df92a90dc1c7826d339
kennethguan1 <kennethguan1@gmail.com>
2021-05-22 21:19:57 -0700

    frontend setup

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+// import techniciansReducer from "./technicians_reducer";
+// import locationsReducer from "./locations_reducer";
+
+const rootReducer = combineReducers({
+//   technicians: techniciansReducer,
+//   locations: locationsReducer,
+});
+
+export default rootReducer;
--- frontend/schedulebud.jsx
+++ frontend/schedulebud.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from './store/store';
 import Root from "./components/root";
+import * as api_util from './util/location_api_util'; 
+
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    window.login = api_util.login;										#used for browser testing
-    window.logout = api_util.logout;
+    window.fetchLocation = api_util.fetchLocation;
+    window.fetchLocations = api_util.fetchLocations;
 
+    const preloadedState = {};
+    let store = configureStore(preloadedState);
 
     const root = document.getElementById("root");
-    ReactDOM.render(<h1>Hello React!</h1>, root);
+    ReactDOM.render(<Root store={store} />, root);
 });
